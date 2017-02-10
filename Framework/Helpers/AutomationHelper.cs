cbb7d87494f0c0fa0594bff0cb37dea8b5b65052
Craig McKay <ukcmcka@waters.com>
2017-02-10 09:12:24 +0000

    add project files

---
+++ Framework/Helpers/AutomationHelper.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Helpers
+{
+    public class AutomationHelper
+    {
+        private static IWebDriver AutomationDriver { get; set; }
+
+        private AutomationHelper()
+        {
+            if (AutomationDriver == null) {
+                CreateAutomationDriver();
+            }
+        }
+
+        public static IWebDriver CreateAutomationDriver()
+        {
+            AutomationDriver = new NgWebDriver(new ChromeDriver());
+
+            //AutomationDriver = new ChromeDriver();
+            return AutomationDriver;
+        }
+    }
+}
---
+++ Specs/StepDefinitions/NewsSteps.cs
@@ -0,0 +1,62 @@
+﻿using Framework.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Specs.StepDefinitions
+{
+    [Binding]
+    public class NewsSteps
+    {
+        private static IWebDriver driver;
+
+        public NewsSteps()
+        {
+            driver = AutomationHelper.CreateAutomationDriver();
+        }
+
+
+        [Given(@"I am on the bbc news website")]
+        public void GivenIAmOnTheBbcNewsWebsite()
+        {
+            driver.Navigate().GoToUrl("http://bbc.co.uk");
+        }
+        
+        [When(@"I access the UK section")]
+        public void WhenIAccessTheUKSection()
+        {
+            driver.FindElement(By.ClassName("navigation-wide-list")).Click();
+            
+            // use page object in future!! 
+        }
+        
+        [When(@"I perform a search for '(.*)'")]
+        public void WhenIPerformASearchFor(string searchTerm)
+        {
+            // use page object in future!! 
+            driver.FindElement(By.Id("orb-search-q")).SendKeys("Trump");
+            driver.FindElement(By.Id("orb-search-button")).Click();
+        }
+        
+        [Then(@"UK related news articles should be displayed")]
+        public void ThenUKRelatedNewsArticlesShouldBeDisplayed()
+        {
+            Assert.IsTrue(driver.Url == "http://bbc.co.uk/news/uk");
+        }
+        
+        [Then(@"news articles relating to Trump should be displayed")]
+        public void ThenNewsArticlesRelatingToTrumpShouldBeDisplayed()
+        {
+            var result = driver.FindElement(By.LinkText("Trump takes office"));
+
+            Assert.IsNotNull(result);
+            //h1[itemprop = headline] > a
+        }
+
+        [AfterFeature]
+        public static void AfterFeature() {
+            if (driver != null) driver.Quit();
+        }
+    }
+}
